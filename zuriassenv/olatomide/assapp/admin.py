b40003c7af5e767324e125b009d1760fcdc5d288
olatomide007 <olatomideafolabi@gmail.com>
2022-06-11 15:26:37 -0700

    first django pro

---
+++ zuriassenv/olatomide/assapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Zurischool, State
+# Register your models here.
+admin.site.register(Zurischool) 
+admin.site.register(State)
---
+++ zuriassenv/olatomide/assapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AssappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'assapp'
